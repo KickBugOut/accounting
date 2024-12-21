b56fd4a39737ee0efe1ff792b5d5992fec3c4ef9
安 杨 <1392178449@qq.com>
2024-12-21 23:10:50 +0800

    V2.0
    中期部署

---
+++ cloudfunctions/install-sdk.bat
@@ -0,0 +1,9 @@
+@echo off
+cd add && npm install wx-server-sdk  && cd.. && ^
+cd delete && npm install wx-server-sdk && cd.. && ^
+cd getDetails && npm install wx-server-sdk && cd.. && ^
+cd getDetailsByNameGroup && npm install wx-server-sdk && cd.. && ^
+cd getDetailsBySubGroup && npm install wx-server-sdk && cd.. && ^
+cd getDetailsByTimeGroup && npm install wx-server-sdk && cd.. && ^
+cd getDetailsByTypeGroup && npm install wx-server-sdk && cd.. && ^
+cd getMoneyDetails && npm install wx-server-sdk && pause
